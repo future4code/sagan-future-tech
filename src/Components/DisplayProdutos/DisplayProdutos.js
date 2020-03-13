31b22a4062b712ce032916effbf7bf3245975e9c
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-13 16:38:05 -0300

    estiliza grid de produtos

--- src/Components/DisplayProdutos/DisplayProdutos.js
+++ src/Components/DisplayProdutos/DisplayProdutos.js
@@ -10,47 +10,49 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
-  card: {
-    maxWidth: 250,
-  },
-  media: {
-    height: 100,
-  },
+	card: {
+		width: 250,
+		height: 310,
+		margin: 10,
+	},
+	media: {
+		height: 120,
+	},
 };
 
 function MediaCard(props) {
-  const { classes } = props;
-  return (
-    <Card id={props.id} onClick={() => {props.mostraPrincipal(props.id)}} className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={props.image}
-          title={props.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.name}
-          </Typography>
-          <Typography component="p">
-          R$ {props.price} - {props.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <Button size="small" color="primary">
-          Ver produto
+	const { classes } = props;
+	return (
+		<Card id={props.id} onClick={() => { props.mostraPrincipal(props.id) }} className={classes.card}>
+			<CardActionArea>
+				<CardMedia
+					className={classes.media}
+					image={props.image}
+					title={props.name}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="h2">
+						{props.name}
+					</Typography>
+					<Typography component="p">
+						R$ {props.price} - {props.description}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+			<CardActions>
+				<Button size="small" color="primary">
+					Ver produto
         </Button>
-      <Button onClick={() => {props.adicionaAoCarrinho(props.id)}} size="small" color="primary">
-          Adicionar ao carrinho
+				<Button onClick={() => { props.adicionaAoCarrinho(props.id) }} size="small" color="primary">
+					Adicionar ao carrinho
         </Button>
-      </CardActions>
-    </Card>
-  );
+			</CardActions>
+		</Card>
+	);
 }
 
 MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(MediaCard);

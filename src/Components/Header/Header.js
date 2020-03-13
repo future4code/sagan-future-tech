033615669a86348b3bd0a64dcde96620a2f3f54b
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-03-13 16:38:40 -0300

    ajusta identação header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -18,193 +18,193 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
+	root: {
+		width: '100%',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing.unit * 2,
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit * 3,
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
 });
 
 class PrimarySearchAppBar extends React.Component {
-  state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
-  };
+	state = {
+		anchorEl: null,
+		mobileMoreAnchorEl: null,
+	};
 
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+	handleProfileMenuOpen = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
+	handleMenuClose = () => {
+		this.setState({ anchorEl: null });
+		this.handleMobileMenuClose();
+	};
 
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
+	handleMobileMenuOpen = event => {
+		this.setState({ mobileMoreAnchorEl: event.currentTarget });
+	};
 
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
-  };
+	handleMobileMenuClose = () => {
+		this.setState({ mobileMoreAnchorEl: null });
+	};
 
-  render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
-    const { classes } = this.props;
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+	render() {
+		const { anchorEl, mobileMoreAnchorEl } = this.state;
+		const { classes } = this.props;
+		const isMenuOpen = Boolean(anchorEl);
+		const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMenuClose}>Minha Conta</MenuItem>
-      </Menu>
-    );
+		const renderMenu = (
+			<Menu
+				anchorEl={anchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMenuOpen}
+				onClose={this.handleMenuClose}
+			>
+				<MenuItem onClick={this.handleMenuClose}>Minha Conta</MenuItem>
+			</Menu>
+		);
 
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </IconButton>
-          <p>Carrinho</p>
-        </MenuItem>
-        <MenuItem onClick={this.acionaAdministracao}>
-          <IconButton color="inherit">
-            <AccountCircle />
-          </IconButton>
-          <p>Vendedor</p>
-        </MenuItem>
-      </Menu>
-    );
+		const renderMobileMenu = (
+			<Menu
+				anchorEl={mobileMoreAnchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMobileMenuOpen}
+				onClose={this.handleMenuClose}
+			>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<IconButton color="inherit">
+						<Badge badgeContent={4} color="secondary">
+							<ShoppingCart />
+						</Badge>
+					</IconButton>
+					<p>Carrinho</p>
+				</MenuItem>
+				<MenuItem onClick={this.acionaAdministracao}>
+					<IconButton color="inherit">
+						<AccountCircle />
+					</IconButton>
+					<p>Vendedor</p>
+				</MenuItem>
+			</Menu>
+		);
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <img src='https://user-images.githubusercontent.com/56808066/76643183-56125180-6533-11ea-852e-e1ff3c6efd9a.png'></img>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                type="text"
-                value={this.props.valorDaBusca}
-                onChange={this.props.acionaOnChange}
-                placeholder="Qual produto deseja?"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
-            </div>
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-              <IconButton onClick={this.props.acionaCarrinho} color="inherit">
-                <Badge badgeContent={this.props.quantidade} color="secondary">
-                  <ShoppingCart />
-                </Badge>
-              </IconButton>
-              <IconButton
-                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                aria-haspopup="true"
-                onClick={this.props.acionaAdministracao}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMenu}
-        {renderMobileMenu}
-      </div>
-    );
-  }
+		return (
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Toolbar>
+						<img src='https://user-images.githubusercontent.com/56808066/76643183-56125180-6533-11ea-852e-e1ff3c6efd9a.png'></img>
+						<div className={classes.search}>
+							<div className={classes.searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								type="text"
+								value={this.props.valorDaBusca}
+								onChange={this.props.acionaOnChange}
+								placeholder="Qual produto deseja?"
+								classes={{
+									root: classes.inputRoot,
+									input: classes.inputInput,
+								}}
+							/>
+						</div>
+						<div className={classes.grow} />
+						<div className={classes.sectionDesktop}>
+							<IconButton onClick={this.props.acionaCarrinho} color="inherit">
+								<Badge badgeContent={this.props.quantidade} color="secondary">
+									<ShoppingCart />
+								</Badge>
+							</IconButton>
+							<IconButton
+								aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+								aria-haspopup="true"
+								onClick={this.props.acionaAdministracao}
+								color="inherit"
+							>
+								<AccountCircle />
+							</IconButton>
+						</div>
+						<div className={classes.sectionMobile}>
+							<IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+								<MoreIcon />
+							</IconButton>
+						</div>
+					</Toolbar>
+				</AppBar>
+				{renderMenu}
+				{renderMobileMenu}
+			</div>
+		);
+	}
 }
 
 PrimarySearchAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(PrimarySearchAppBar);
